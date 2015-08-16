8714cbdbf9a69cdc377cef395a005b8ef4026639
hillsreddy <hillsreddy@gmail.com>
2015-08-16 03:39:17 -0700

    Drools with Jpa and Maven

---
+++ JpaDrools/src/main/java/com/practice/drools/DroolsTest.java
@@ -0,0 +1,83 @@
+package com.practice.drools;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+/* This is a sample class to launch a rule. */
+
+public class DroolsTest {
+	public static final void main(String[] args) {
+		try {
+			EntityManagerFactory emfactory = Persistence
+					.createEntityManagerFactory("free");
+			EntityManager entitymanager = emfactory.createEntityManager();
+
+			FreedomPK pk = new FreedomPK();
+			pk.setContinent("AMERICA");
+			pk.setCountry("US");
+			Freedom item1 = entitymanager.find(Freedom.class, pk);
+
+			System.out.println("FreedomPK = " + item1.getState());
+
+			// Employee emp = entitymanager.find( Employee.class, 1201 );
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase
+					.newStatefulKnowledgeSession();
+
+			// Freedom item1 = new Freedom();
+			// item1.setState(item1.getState());
+			// item1.setDate(item1.getDate());
+			ksession.insert(item1);
+
+			// emp.setEid(1);
+			// emp.setEname(emp.getEname());
+			// emp.setSalary(emp.getSalary());
+			// emp.setDeg(emp.getDeg());
+			// ksession.insert(emp);
+
+			ksession.fireAllRules();
+
+			System.out.println(item1.getState() + " " + item1.getDate());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+
+		kbuilder.add(ResourceFactory.newClassPathResource("helloWorld1.drl"),
+				ResourceType.DRL);
+		// kbuilder.add(ResourceFactory.newClassPathResource("Nagpur.drl"),
+		// ResourceType.DRL);
+
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error : errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+
+		return kbase;
+	}
+}
---
+++ JpaDrools/src/main/java/com/practice/drools/Freedom.java
@@ -0,0 +1,53 @@
+package com.practice.drools;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+
+/**
+ * The persistent class for the freedom database table.
+ * 
+ */
+@Entity
+@NamedQuery(name = "Freedom.findAll", query = "SELECT f FROM Freedom f")
+public class Freedom implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private FreedomPK id;
+
+	private BigDecimal date;
+
+	private String state;
+
+	public Freedom() {
+	}
+
+	public FreedomPK getId() {
+		return this.id;
+	}
+
+	public void setId(FreedomPK id) {
+		this.id = id;
+	}
+
+	public BigDecimal getDate() {
+		return this.date;
+	}
+
+	public void setDate(BigDecimal date) {
+		this.date = date;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
