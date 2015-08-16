93d03a3e1b679b14453944c56d2e28c3962a1caa
hillsreddy <hillsreddy@gmail.com>
2015-08-16 07:39:27 -0700

    Drools Validations using JPA & Maven

---
+++ DroolsValidations/src/main/java/com/practice/drools/RulesManager.java
@@ -0,0 +1,91 @@
+package com.practice.drools;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import com.practice.drools.jpa.entity.ValidationControl;
+import com.practice.drools.jpa.entity.ValidationErrors;
+
+/* This is a sample class to launch a rule. */
+
+public class RulesManager {
+	public static final void main(String[] args) {
+		try {
+			EntityManagerFactory emfactory = Persistence
+					.createEntityManagerFactory("validations");
+			EntityManager entitymanager = emfactory.createEntityManager();
+			entitymanager.getTransaction( ).begin( );
+			
+			FreedomPK pk = new FreedomPK();
+			pk.setContinent("AMERICA");
+			pk.setCountry("US");
+			Freedom item1 = entitymanager.find(Freedom.class, pk);
+			
+			ValidationControl control = entitymanager.find(ValidationControl.class, 1);
+			
+			ValidationErrors error = new ValidationErrors();
+		    //error.setErrorSeqNbr(1);
+		    error.setErrorMsg("state is empty");
+		    error.setCreatedBy("Reddy");
+		    error.setCreatedDate(16082015);
+		    error.setCreatedTime(1646);
+			
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase
+					.newStatefulKnowledgeSession();
+
+			ksession.insert(item1);
+			ksession.insert(control);
+			ksession.insert(error);
+
+			ksession.fireAllRules();
+			
+			entitymanager.persist(error);
+		      entitymanager.getTransaction( ).commit( );
+
+		      entitymanager.close( );
+		      emfactory.close( );
+
+			System.out.println(item1.getState() + " " + item1.getDate());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+
+		kbuilder.add(ResourceFactory.newClassPathResource("validations-001-099.drl"),
+				ResourceType.DRL);
+		kbuilder.add(ResourceFactory.newClassPathResource("validations-100-199.drl"), ResourceType.DRL);
+
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error : errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+
+		return kbase;
+	}
+}
---
+++ DroolsValidations/src/main/java/com/practice/drools/jpa/entity/ValidationControl.java
@@ -0,0 +1,65 @@
+package com.practice.drools.jpa.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the validation_control database table.
+ * 
+ */
+@Entity
+@Table(name="validation_control")
+@NamedQuery(name="ValidationControl.findAll", query="SELECT v FROM ValidationControl v")
+public class ValidationControl implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="VALIDATION_NO")
+	private int validationNo;
+
+	@Column(name="CREATED_BY")
+	private String createdBy;
+
+	@Column(name="CREATED_DATE")
+	private int createdDate;
+
+	@Column(name="CREATED_TIME")
+	private int createdTime;
+
+	public ValidationControl() {
+	}
+
+	public int getValidationNo() {
+		return this.validationNo;
+	}
+
+	public void setValidationNo(int validationNo) {
+		this.validationNo = validationNo;
+	}
+
+	public String getCreatedBy() {
+		return this.createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public int getCreatedDate() {
+		return this.createdDate;
+	}
+
+	public void setCreatedDate(int createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public int getCreatedTime() {
+		return this.createdTime;
+	}
+
+	public void setCreatedTime(int createdTime) {
+		this.createdTime = createdTime;
+	}
+
+}
