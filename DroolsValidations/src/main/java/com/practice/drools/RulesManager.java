9feefc5306e961f5549a58d2298365db33e63c72
hillsreddy <hillsreddy@gmail.com>
2015-08-16 07:58:44 -0700

    code modified

--- DroolsValidations/src/main/java/com/practice/drools/RulesManager.java
+++ DroolsValidations/src/main/java/com/practice/drools/RulesManager.java
@@ -25,22 +25,23 @@ public class RulesManager {
 			EntityManagerFactory emfactory = Persistence
 					.createEntityManagerFactory("validations");
 			EntityManager entitymanager = emfactory.createEntityManager();
-			entitymanager.getTransaction( ).begin( );
-			
+			entitymanager.getTransaction().begin();
+
 			FreedomPK pk = new FreedomPK();
 			pk.setContinent("AMERICA");
 			pk.setCountry("US");
 			Freedom item1 = entitymanager.find(Freedom.class, pk);
-			
-			ValidationControl control = entitymanager.find(ValidationControl.class, 1);
-			
+
+			ValidationControl control = entitymanager.find(
+					ValidationControl.class, 1);
+
 			ValidationErrors error = new ValidationErrors();
-		    //error.setErrorSeqNbr(1);
-		    error.setErrorMsg("state is empty");
-		    error.setCreatedBy("Reddy");
-		    error.setCreatedDate(16082015);
-		    error.setCreatedTime(1646);
-			
+			// error.setErrorSeqNbr(1);
+			error.setErrorMsg("state is empty");
+			error.setCreatedBy("Reddy");
+			error.setCreatedDate(16082015);
+			error.setCreatedTime(1646);
+
 			// load up the knowledge base
 			KnowledgeBase kbase = readKnowledgeBase();
 			StatefulKnowledgeSession ksession = kbase
@@ -51,12 +52,12 @@ public class RulesManager {
 			ksession.insert(error);
 
 			ksession.fireAllRules();
-			
-			entitymanager.persist(error);
-		      entitymanager.getTransaction( ).commit( );
 
-		      entitymanager.close( );
-		      emfactory.close( );
+			entitymanager.persist(error);
+			entitymanager.getTransaction().commit();
+
+			entitymanager.close();
+			emfactory.close();
 
 			System.out.println(item1.getState() + " " + item1.getDate());
 
@@ -70,9 +71,12 @@ public class RulesManager {
 		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
 				.newKnowledgeBuilder();
 
-		kbuilder.add(ResourceFactory.newClassPathResource("validations-001-099.drl"),
+		kbuilder.add(
+				ResourceFactory.newClassPathResource("validations-001-099.drl"),
 				ResourceType.DRL);
-		kbuilder.add(ResourceFactory.newClassPathResource("validations-100-199.drl"), ResourceType.DRL);
+		kbuilder.add(
+				ResourceFactory.newClassPathResource("validations-100-199.drl"),
+				ResourceType.DRL);
 
 		KnowledgeBuilderErrors errors = kbuilder.getErrors();
 
